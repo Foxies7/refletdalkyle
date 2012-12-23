348b8e9940d81e1642c86977c77470e81e9541a3
Enshiro <enhrim@gmail.com>
2012-12-23 20:07:41 +0100

    Re
    version qui fonctionne  un peu mieux (image ralentit mais vers le bas on
    dirait qu'il boite!)

--- Jeu/WindowsGame2/WindowsGame2/Game.cs
+++ Jeu/WindowsGame2/WindowsGame2/Game.cs
@@ -20,7 +20,7 @@ namespace WindowsGame2
         private Texture2D link0;        //on créé un attribut
         private int Largeur;
         private int Longueur;
-        private int Speed = 2;
+        private int Speed = 1;
         private int Droite = 0;
         private int Haut = 0;
         private int Bas = 0;
@@ -88,71 +88,78 @@ namespace WindowsGame2
        
             if (keyboard.IsKeyDown(Keys.Up))                   //si la touche up est enfoncée...
             {
-                if (Haut == 0)
+                if ((Haut == 0) || (Haut == 1) || (Haut == 2))
                 {
                     link0 = Content.Load<Texture2D>("1");
                     Haut++;
                 }
 
-                else if (Haut == 1)
+                else if ((Haut == 3) || (Haut == 4) || ( Haut == 5))
                 {
                     link0 = Content.Load<Texture2D>("2");
                     Haut++;
                 }
 
-                else if (Haut == 2)
+                else if ((Haut == 6) || (Haut == 7) || (Haut == 8))
                 {
                     link0 = Content.Load<Texture2D>("3");
                     Haut++;
                 }
 
-                else if (Haut == 3)
+                else if ((Haut == 9) || (Haut == 10) || (Haut == 11))
                 {
                     link0 = Content.Load<Texture2D>("4");
                     Haut++;
                 }
 
-                else if (Haut == 4)
+                else if ((Haut == 12) || (Haut == 13) || (Haut == 14))
                 {
                     link0 = Content.Load<Texture2D>("5");
                     Haut++;
                 }
 
-                else if (Haut == 5)
+                else if ((Haut == 15) || (Haut == 16) || (Haut == 17))
                 {
                     link0 = Content.Load<Texture2D>("6");
                     Haut++;
                 }
 
-                else if (Haut == 6)
+                else if ((Haut == 18) || (Haut == 19) || (Haut == 20))
                 {
                     link0 = Content.Load<Texture2D>("7");
                     Haut++;
                 }
 
-                else if (Haut == 7)
+                else if ((Haut == 21) || (Haut == 22) || (Haut == 23))
                 {
                     link0 = Content.Load<Texture2D>("8");
                     Haut++;
                 }
 
-                else if (Haut == 8)
+                else if ((Haut == 24) || (Haut == 25) || (Haut == 26))
                 {
                     link0 = Content.Load<Texture2D>("9");
                     Haut++;
                 }
 
-                else if (Haut == 9)
+                else if ((Haut == 27) || (Haut == 28) || (Haut == 29))
                 {
                     link0 = Content.Load<Texture2D>("10");
                     Haut++;
                 }
 
-                else if (Haut ==10)
+                else if ((Haut == 30) || (Haut == 31))
                 {
                     link0 = Content.Load<Texture2D>("11");
-                    Haut -= 10;
+                    Haut++;
                 }
+
+                else if (Haut == 32)
+                {
+                    link0 = Content.Load<Texture2D>("11");
+                    Haut -= 32;
+                }
+                    
                     Position.Y -= Speed;
             }
 #endregion
@@ -161,65 +168,60 @@ namespace WindowsGame2
 
             if (keyboard.IsKeyDown(Keys.Down))
             {
-                if (Bas == 0)
+                if ((Bas == 0) || (Bas == 1) || (Bas == 2))
                 {
                     link0 = Content.Load<Texture2D>("23");
                     Bas++;
                 }
 
-                else if (Bas == 1)
+                else if ((Bas == 3) || (Bas == 4) || (Bas == 5))
                 {
                     link0 = Content.Load<Texture2D>("24");
                     Bas++;
                 }
 
-                else if (Bas == 2)
+                else if ((Bas == 6) || (Bas == 7) || (Bas == 8))
                 {
                     link0 = Content.Load<Texture2D>("25");
                     Bas++;
                 }
 
-                else if (Bas == 3)
+                else if ((Bas == 9) || (Bas == 10) || (Bas == 11))
                 {
                     link0 = Content.Load<Texture2D>("26");
                     Bas++;
                 }
 
-                else if (Bas == 4)
+                else if ((Bas == 12) || (Bas == 13) || (Bas == 14))
                 {
                     link0 = Content.Load<Texture2D>("27");
                     Bas++;
                 }
 
-                else if (Bas == 5)
+                else if ((Bas == 15) || ( Bas == 16) || (Bas == 17))
                 {
                     link0 = Content.Load<Texture2D>("28");
                     Bas++;
                 }
 
-                else if (Bas == 6)
+                else if ((Bas == 18) || (Bas == 19) || (Bas == 20))
                 {
                     link0 = Content.Load<Texture2D>("29");
                     Bas++;
                 }
 
-                else if (Bas == 7)
+                else if ((Bas == 21) || (Bas == 22))
                 {
                     link0 = Content.Load<Texture2D>("30");
                     Bas++;
                 }
 
-                else if (Bas == 8)
+                else if (Bas == 23)
                 {
-                    link0 = Content.Load<Texture2D>("31");
-                    Bas++;
+                    link0 = Content.Load<Texture2D>("30");
+                    Bas -= 23;
                 }
 
-                else if (Bas == 9)
-                {
-                    link0 = Content.Load<Texture2D>("32");
-                    Bas -= 9;
-                }
 
                     Position.Y += Speed;
             }
@@ -230,70 +232,64 @@ namespace WindowsGame2
             #region -Mouvements à Droite-
             if (keyboard.IsKeyDown(Keys.Right))                
             {
-                if (Droite == 0)
+                if ((Droite == 0) || (Droite == 1) || (Droite == 2))
                 {
                     link0 = Content.Load<Texture2D>("12");
                     Droite++;
                 }
 
-                else if (Droite == 1)
+                else if ((Droite == 3) || (Droite == 4) || (Droite == 5))
                 {
                     link0 = Content.Load<Texture2D>("13");
                     Droite++;
                 }
 
-                else if (Droite == 2)
+                else if ((Droite == 6) || (Droite == 7) || (Droite == 8))
                 {
                     link0 = Content.Load<Texture2D>("14");
                     Droite++;
                 }
 
-                else if (Droite == 3)
+                else if ((Droite == 9) || (Droite == 10) || (Droite == 11))
                 {
                     link0 = Content.Load<Texture2D>("15");
                     Droite++;
                 }
 
-                else if (Droite == 4)
+                else if ((Droite == 12) || (Droite == 13) || (Droite == 14))
                 {
                     link0 = Content.Load<Texture2D>("16");
                     Droite++;
                 }
 
-                else if (Droite == 5)
+                else if ((Droite == 15) || (Droite == 16) || (Droite == 17))
                 {
                     link0 = Content.Load<Texture2D>("17");
                     Droite++;
                 }
 
-                else if (Droite == 6)
+                else if ((Droite == 18) || (Droite == 19) || (Droite == 20))
                 {
                     link0 = Content.Load<Texture2D>("18");
                     Droite++;
                 }
 
-                else if (Droite == 7)
+                else if ((Droite == 21) || (Droite == 22) || (Droite == 23))
                 {
                     link0 = Content.Load<Texture2D>("19");
                     Droite++;
                 }
 
-                else if (Droite == 8)
+                else if ((Droite == 24) || (Droite == 25))
                 {
                     link0 = Content.Load<Texture2D>("20");
                     Droite++;
                 }
 
-                else if (Droite == 9)
+                else if (Droite == 26)
                 {
-                    link0 = Content.Load<Texture2D>("21");
-                    Droite++;
-                }
-
-                else if (Droite == 10)
-                {
-                    link0 = Content.Load<Texture2D>("22");
-                    Droite -= 10;
+                    link0 = Content.Load<Texture2D>("20");
+                    Droite -= 26;
                 }
                     Position.X += Speed;
             }
